9aa552bc7983d297d7e0130e2767f8635e9cdbdc
Melodie Malfa <melmalfa@gmail.com>
2014-12-10 08:40:32 -0500

    finishes hw

--- w02/d02/student/apartment.rb
+++ w02/d02/student/apartment.rb
@@ -1,3 +1,4 @@
+require_relative '.tenant'
 
 class Apartment
 
@@ -16,18 +17,25 @@ b4 = Apartment.new("12d", 3, 2, 3)
 
 end
 
+apartment_price = ["a1", "a2", "b4"]
+# i know this is cheating, i need the array to be populated based on whatever new values might be entered for apartments. (how?) but will try this for now to see if at least on right track with formula.
 
-def apt_price()
+apartment_price = (Apartment[1]*1000) + (Apartment[3]*500)
 
-end
+# or
+
+# the following doesn't work
+# Apartment.price do |idk|
+#   (Apartment[1]*1000) + (Apartment[3]*500)
+# end
 
 
-An **apartment's** price should be dynamically calculated based on the number of beds ($1000 per bed) and the number of bathrooms ($500 per bathroom).
+# An **apartment's** price should be dynamically calculated based on the number of beds ($1000 per bed) and the number of bathrooms ($500 per bathroom).
 
-Example Usage:
+# Example Usage:
 
-```rb
-a2 = Apartment.new("A", 2, 1)
-a2.price #=> "$2500"
+# ```rb
+# a2 = Apartment.new("A", 2, 1)
+# a2.price #=> "$2500"
 
 
--- w02/d02/student/tenant.rb
+++ w02/d02/student/tenant.rb
@@ -1,15 +1,17 @@
+require_relative '/.apartment'
+
 class Tenant
 
-attr accessor :f_name, :l_name, :born_on, :gender
-
-  def initialize(f_name, l_name, born_on, gender)
+  def initialize(f_name, l_name, born_on, gender, nickname, occupation)
     @f_name = f_name
     @l_name = l_name
     @born_on = born_on
     @gender = gender
+    @nickname = nickname
+    @occupation = occupation
   end
 
-  def initialize(nickname, occupation)
+  attr_accessor :f_name, :l_name, :born_on, :gender
 
     def nickname=(a)
       @nickname = a
@@ -26,22 +28,13 @@ attr accessor :f_name, :l_name, :born_on, :gender
     def occupation
       @occupation
     end
-end
+  end
 
-A **tenant** should have a `full_name` method that returns a string of their first name, last name and nickname (if they have one).
-
-p1 = Tenant.new()
-p1.full_name
+p1 = Tenant.new("Brian", "Rucker", "male", "March 20, 1992", "Brucker", "student")
+p2 = Tenant.new("Melodie", "Malfa", "female", "March 3, 1977", "Mel", "student")
+p3 = Tenant.new("Nancy", "Drew", "female", "November 6, 1932", "Slueth". "detective")
+p4 = Tenant.new("bell", "hooks", "female", "May 01, 1956", "kick-ass", "cultural critic")
 
 def full_name go
   "f_name" + " nickname" + " l_name"
 end
-
-
-Example Usage:
-
-```rb
-p1 = Tenant.new("Calvin", "Clifford", "male", "December 21, 1929")
-p1.nickname = "Bud"
-p1.full_name #=> "Calvin 'Bud' Clifford"
-
