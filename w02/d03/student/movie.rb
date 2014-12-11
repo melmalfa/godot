534c02e11dcb615b4dba3202208e9e5ddc0e0cb8
Melodie Malfa <melmalfa@gmail.com>
2014-12-10 20:59:17 -0500

    tries part 3 again

--- w02/d03/student/movie.rb
+++ w02/d03/student/movie.rb
@@ -1,35 +1,44 @@
-class Movie
-
-attr_accessor :title
-  def initialize (title)
-  end
-
-end
-
-
-
-
-
 # class Movie
 
-#   attr_accessor :title, :director
-
-#   def initialize (title, director)
-#     @title = title
-#     @director = director
+# attr_accessor :title
+#   def initialize (title)
 #   end
 
 # end
 
-# class Person
-# end
 
 
-# movie = Movie.new ("Jaws")
 
-# movie.director
 
-# movie.director = "Richard Ayoade"
+class Movie
+
+  attr_accessor :title
+
+  def initialize (title)
+    @title = title
+  end
+
+end
+
+class Person
+
+  attr_accessor :director
+
+  def initialize (director)
+
+  end
+end
+
+
+movie = Movie.new ("Jaws")
+
+movie.director
+
+movie = Person.new ("richard")
+
+movie.director = "Richard Ayoade"
+
+
 
 # hey instructors! so I made part 1 and 2 work perfectly - yay! and now i realize i don;t know how you could see this, since i had to change and add and remove to keep moving on. i got to this last part of 3 and i couldn't figure out how to to the following (create a person object returned by movie.director) but i tried a bunch of different things, so now i am - yes- giving up and moving on to next problem as i want to learn and not bang my head against the . . . table. :)
 
