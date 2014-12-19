d48546b29b2e4d4c6eea751c495ed06ef50989f7
Melodie Malfa <melmalfa@gmail.com>
2014-12-19 12:27:01 -0500

    completes pair_programming_bot

---
+++ w03/d05/student/pair_programming_bot/server.rb
@@ -0,0 +1,38 @@
+module PairProgrammingBot
+  class Server < Sinatra::Base
+
+
+    get('/') do
+      render(:erb, :index, { :layout => :default_layout })
+    end
+
+    get('/pass') do
+      render(:erb, :pass, { :layout => :default_layout })
+    end
+
+    get('/write_test') do
+      render(:erb, :write_test, { :layout => :default_layout })
+    end
+
+    get('/refactor') do
+      render(:erb, :refactor, { :layout => :default_layout })
+    end
+
+    get('/write_code') do
+      render(:erb, :write_code, { :layout => :default_layout })
+    end
+
+    get('/do_refactor') do
+      render(:erb, :do_refactor, { :layout => :default_layout })
+    end
+
+    get('/new_feature') do
+      render(:erb, :new_feature, { :layout => :default_layout })
+    end
+
+
+
+
+
+  end
+end
