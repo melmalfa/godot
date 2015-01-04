af130b720df2ed536d078d90802e6225d2b801f6
Melodie Malfa <melmalfa@gmail.com>
2015-01-04 11:02:33 -0500

    makes some changes to lars

--- w02/d01/student/bond_answers.rb
+++ w02/d01/student/bond_answers.rb
@@ -23,3 +23,16 @@ end
 
 # 1. Similarly, create a variable called `highest_grossing_bond` and use your programming powers to return the highest grossing film hash, storing it to `highest_grossing_bond`.
 
+# Create a new array with the names of the all actors who have ever played Bond, but with no duplicates. (One Roger Moore is more than enough.) Assign it to a sensibly-named variable.
+
+# Create a new array of strings from BOND_DATA with the names of al the Bond films. Assign it to a sensibly-named variable.
+
+# Create a new array of hashes of only the bond films released on odd-numbered years. Assign it to a sensibly-named variable.
+
+
+# Create a variable called worst_grossing_bond. Use all the tools at your disposal (and perhaps one or two you have yet to discover) to iterate through all the films and return the film with the lowest gross. Store the returned film hash in the variable worst_grossing_bond.
+
+
+
+
+
