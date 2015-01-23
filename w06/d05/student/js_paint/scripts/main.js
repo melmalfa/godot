f0310923e44000ec95de35612cccdae5df658871
Melodie Malfa <melmalfa@gmail.com>
2015-01-23 16:16:01 -0500

    finishes pt 2 hw

--- w06/d05/student/js_paint/scripts/main.js
+++ w06/d05/student/js_paint/scripts/main.js
@@ -2,5 +2,23 @@ var mouseDepressed = false;
 
 var body = document.querySelector("body");
 
+var mouseIn = function(event) {
+    event.target.setAttribute("class", "tile green");
+  }
+
+var mouseOut = function(event) {
+    event.target.setAttribute("class", "tile");
+  }
+
+var canvas = document.querySelector(".canvas");
+for (var i = 0; i < 805; i++) {
+  var lots = document.createElement("div");
+  lots.setAttribute("class", "tile");
+  lots.addEventListener("mouseover", mouseIn);
+  lots.addEventListener("mouseout", mouseOut);
+  canvas.appendChild(lots);
+}
+
+
 
 
--- w06/d05/student/the_terminator/scripts/main.js
+++ w06/d05/student/the_terminator/scripts/main.js
@@ -1,1 +1,35 @@
 console.log("main.js linked");
+
+var inhabitants = document.querySelector("h1");
+
+inhabitants.textContent = "Inhabitants of Los Angeles (1984)"
+
+var cops = document.getElementsByClassName("cop collateral-damage")
+// var cops = document.querySelectorAll(".cop");
+
+for (var i = 0; i < cops.length; i++) {
+  cops.color = "lightskyblue";
+}
+
+//for(var i = 0; i < cops.length; i++) {
+//   cops[i].style.color = 'lightskyblue';
+// }
+
+var terminator = document.createElement("div")
+terminator.className = "terminator"
+document.querySelector("#los-angeles").appendChild(terminator)
+
+var OneTrueSarahConnor = document.querySelector("#the-one-true-sarah-connor")
+
+OneTrueSarahConnor.className = "badass"
+//OneTrueSarahConnor.setAttribute("class", "badass");
+
+var poorSouls = document.querySelectorAll(".collateral-damage")
+
+for(var i = 0; i < poorSouls.length; i++) {
+  poorSouls[i].parentNode.removeChild(poorSouls[i]);
+}
+
+
+// Iterate through poorSouls, removing the elements from the DOM one by one.
+// Remove the terminator from the DOM.
