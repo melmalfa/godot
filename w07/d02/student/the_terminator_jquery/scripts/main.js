085c079390307b044765dd89c06018f666d0fe93
Melodie Malfa <melmalfa@gmail.com>
2015-01-27 19:06:59 -0500

    finishes hw

--- w07/d02/student/the_terminator_jquery/scripts/main.js
+++ w07/d02/student/the_terminator_jquery/scripts/main.js
@@ -1,7 +1,13 @@
 console.log("main.js linked");
-
 $(document).ready(function(){
-
-  // Students! Write your playful jQuery code here!
-
+  var inhabitants = $("h1");
+  $(inhabitants).text("Inhabitants of Los Angeles (1984)");
+  cops = $(".cop");
+  $(cops).css("color", "lightskyblue");
+  $("div#los-angeles").append("<div class='terminator'></div>");
+  var theOneTrueSarahConnor = $("#the-one-true-sarah-connor");
+  $(theOneTrueSarahConnor).addClass("badass");
+  var poorSouls = $(".collateral-damage");
+  //not sure the following removes them one by one, though according to the docs this format has an implicit each loop- does that mean it happens one by one?
+  $(poorSouls).remove("li");
 });
