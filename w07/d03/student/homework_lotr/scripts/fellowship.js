d6fb4f6dc140693aedb44943569a0abdc761b8e9
Melodie Malfa <melmalfa@gmail.com>
2015-01-28 19:02:58 -0500

    begins hw

--- w07/d03/student/homework_lotr/scripts/fellowship.js
+++ w07/d03/student/homework_lotr/scripts/fellowship.js
@@ -1,1 +1,19 @@
 console.log("followship.js loaded");
+
+var $body = $("body");
+var $lands = lands;
+
+
+var makeMiddleEarth = function() {
+  $("<section id=\"middle-earth\">");
+  for ( var i = 0, len = lands.length; i < len; i++) {
+    var $land = $("<article>");
+    $land.addClass("land");
+    $land.append("<h1>");
+    var $h1 = $("h1");
+    $h1.textContent("$lands[i]");
+  }
+
+}
+
+setTimeout(makeMiddleEarth, 2000)
