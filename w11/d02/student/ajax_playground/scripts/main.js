406950b1c46d8b7ff8e09ca8ae5680d86c3e7ffc
Melodie Malfa <melmalfa@gmail.com>
2015-02-24 15:47:15 -0500

    commits todays work

--- w11/d02/student/ajax_playground/scripts/main.js
+++ w11/d02/student/ajax_playground/scripts/main.js
@@ -3,5 +3,55 @@ $(document).ready(function(){
 
   // caches reference to body element
   var $body = $("body");
+  $("#random-user-btn").on("click", function(){
+    $.ajax({
+        url: "http://api.randomuser.me",
+        type: "GET",
+        dataType: "json"
+      }).done(function(data){
+      var user = data["results"][0]["user"];
+      var pictureUrl = user["picture"]["medium"];
+      var image = $("<img>").attr("src", pictureUrl);
+      $("body").append(image);
+  // debugger
+    });
+  });
+
+  $("#female-user-btn").on("click", function(){
+  $.ajax({
+    url: "http://api.randomuser.me",
+    type: "GET",
+    // client-side params hash
+    //best to make a function for the data info you want to pass in
+    data: { gender: "female" },
+    dataType: "json"
+      }).done(function(data){
+      var user = data["results"][0]["user"];
+      var pictureUrl = user["picture"]["medium"];
+      var image = $("<img>").attr("src", pictureUrl);
+      $("body").append(image);
+    });
+  });
+
+   $("#male-user-btn").on("click", function(){
+    $.ajax({
+    url: "http://api.randomuser.me",
+    type: "GET",
+    // client-side params hash
+    //best to make a function for the data info you want to pass in
+    data: { gender: "male" },
+    dataType: "json"
+      }).done(function(data){
+      var user = data["results"][0]["user"];
+      var pictureUrl = user["picture"]["medium"];
+      var image = $("<img>").attr("src", pictureUrl);
+      $("body").append(image);
+    });
+  });
+
 
 });
+
+
+
+
--- w11/d02/student/weather_vain/scripts/main.js
+++ w11/d02/student/weather_vain/scripts/main.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
   console.log("document ready");
 
+  var $forecast = $("#forecast")
+
+
+  $(".pure-button").on("click", function(e){
+    $city = $("#pure-input-rounded").val();
+    e.preventDefault();
+    $.ajax({
+    url: "http://api.openweathermap.org/data/2.5/weather",
+    type: "GET",
+    data: { q: $city, units: "imperial" },
+    dataType: "json"
+    }).done(function(data){
+      var temp = data.main.temp
+      // data['list'][0]['main']['temp']
+      $forecast.append("In "+$city+" , it's "+temp+" degrees Farenheit.");
+    });
+  });
+
   // sets the day's date at the bottom of viewport
   var $date = $(".date");
   var currentDateString = moment().format('dddd, MMMM Do');
